cea8c50f5430af5859aef48cd448bf005bbfa73c
Guilherme Silva <guisilva.master@gmail.com>
2024-02-08 08:35:14 -0300

    chore: add title to product details page

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -20,13 +20,13 @@ export default function Product() {
 
   const product = PRODUCTS.find((product) => product.id === id);
 
-  if (!product) {
-    return <Redirect href="/" />
-  }
-
   function handleAddToCart() {
     cartStore.add(product!)
     navigation.goBack();
+  }
+
+  if (!product) {
+    return <Redirect href="/" />
   }
 
   return (
@@ -34,6 +34,7 @@ export default function Product() {
       <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
