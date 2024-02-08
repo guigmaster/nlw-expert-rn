24257c007d979db796a78ae112155d1deacc86d8
Guilherme Silva <guisilva.master@gmail.com>
2024-02-07 23:42:25 -0300

    chore: add product list into home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
-import { FlatList, View } from "react-native";
+import { useRef, useState } from "react";
+import { FlatList, SectionList, Text, View } from "react-native";
 
-import { CATEGORIES } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
+import { Product } from "@/components/product";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
 
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
+
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    );
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
   }
 
   return (
@@ -31,6 +46,22 @@ export default function Home() {
             onPress={() => handleCategorySelect(item)}
           />
         )}
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
+        )}
+        renderItem={({ item }) => (
+          <Product data={item} />
+        )}
+        className="flex-1 p-5"
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   )
