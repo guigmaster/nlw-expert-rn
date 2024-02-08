03f37f6a1e43c223e811159631834417482a28c8
Guilherme Silva <guisilva.master@gmail.com>
2024-02-07 22:26:25 -0300

    chore: create layout and first page

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1">
+      <Text className="text-white">Hello from NLW</Text>
+    </View>
+  )
+}
