a6dd7e426ca2f1ad801120a04b38df05eb14edde
Guilherme Silva <guisilva.master@gmail.com>
2024-02-09 21:40:53 -0300

    chore: add persist middlewaer to zustand

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,4 +1,7 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 import { ProductProps } from "@/utils/data/products";
 
@@ -14,12 +17,20 @@ type StateProps = {
   remove: (productId: string) => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) => set((state) => ({
-    products: cartInMemory.add(state.products, product)
-  })),
-  remove: (productId: string) => set((state) => ({
-    products: cartInMemory.remove(state.products, productId)
-  })),
-}));
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
+    products: [],
+
+    add: (product: ProductProps) => set((state) => ({
+      products: cartInMemory.add(state.products, product)
+    })),
+    
+    remove: (productId: string) => set((state) => ({
+      products: cartInMemory.remove(state.products, productId)
+    })),
+  }),
+  {
+    name: "",
+    storage: createJSONStorage(() => AsyncStorage)
+  })  
+);
